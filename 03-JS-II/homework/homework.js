75fc4a39a4ff616a5673481425b50b4c82bb3351
= <=>
2022-04-08 15:53:53 -0300

    2nd homework

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -130,7 +130,7 @@ function fizzBuzz(numero) {
     return 'fizz';
   } else if (numero % 5 == 0) {
     return 'buzz';
-  } else if (numero % 3 == 0 && numero % 5 == 0) {
+  } else if (numero % 15 == 0) {
     return 'fizzbuzz';
   } else {
     return numero;
@@ -144,14 +144,14 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if (num1 > num2 && num1 > num3 && num > 0) {
+  if (num1 > num2 && num1 > num3 && num1 > 0) {
     return 'Número 1 es mayor y positivo';
   } else if (num1 < 0 || num2 < 0 || num3 < 0) {
     return 'Hay negativos';
   } else if (num3 > num1 && num3 > num2) {
     num3 += 1;
     return num3;
-  } else if (num1 == 0 || num2 == 0 || num3 == 0) {
+  } else if (num1 === 0 || num2 === 0 || num3 === 0) {
     return 'Error';
   } else {
     return false;
@@ -184,8 +184,7 @@ function tablaDelSeis(){
   //Escribe tu código aquí   
   let arrayTablaDelSeis = [];
   for (i = 0; i <= 10; i++) {
-    i = i * 6;
-    arrayTablaDelSeis.push(i);
+    arrayTablaDelSeis.push(i * 6);
   }
   return arrayTablaDelSeis;
 }
@@ -193,7 +192,7 @@ function tablaDelSeis(){
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  if (numero.length == 3) {
+  if (numero >= 100 && numero <= 999) {
     return true;
   } else {
     return false;

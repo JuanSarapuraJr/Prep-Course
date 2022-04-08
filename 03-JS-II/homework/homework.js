4c0291a2436a4e8270b0ebfb3707abf2ca815691
= <=>
2022-04-08 13:29:56 -0300

    Second homework

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -18,7 +18,7 @@ function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
-  if (edad > 18) {
+  if (edad >= 18) {
     return 'Allowed';
   } else {
     return 'Not allowed';
@@ -31,9 +31,9 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
-  if (status === 1) {
+  if (status == 1) {
     return 'Online';
-  } else if (status === 2) {
+  } else if (status == 2) {
     return 'Away';
   } else {
     'Offline';
@@ -48,7 +48,7 @@ function saludo(idioma) {
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
   if (idioma === 'aleman') {
-    return 'Guten Tag';
+    return 'Guten Tag!';
   } else if (idioma === 'mandarin') {
     return 'Ni Hao!';
   } else if (idioma === 'ingles') {

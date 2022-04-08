d21d9eedb5cb838c5d22a99d264a5bd7af67b1e1
= <=>
2022-04-08 16:32:15 -0300

    Second homework

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -126,12 +126,12 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
-  if (numero % 3 == 0) {
-    return 'fizz';
-  } else if (numero % 5 == 0) {
-    return 'buzz';
-  } else if (numero % 15 == 0) {
+  if (numero % 3 === 0 && numero % 5 === 0) {
     return 'fizzbuzz';
+  } else if (numero % 5 === 0) {
+    return 'buzz';
+  } else if (numero % 3 === 0) {
+    return 'fizz';
   } else {
     return numero;
   }
@@ -144,16 +144,16 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
-  if (num1 > num2 && num1 > num3 && num1 > 0) {
-    return 'Número 1 es mayor y positivo';
-  } else if (num1 < 0 || num2 < 0 || num3 < 0) {
+  if if (num1 === 0 || num2 === 0 || num3 === 0) {
+    return 'Error';
+  } else if(num1 < 0 || num2 < 0 || num3 < 0) {
     return 'Hay negativos';
-  } else if (num3 > num1 && num3 > num2) {
+  } else if {(num1 > num2 && num1 > num3 && num1 > 0) {
+    return 'Número 1 es mayor y positivo';
+  } else if(num3 > num1 && num3 > num2) {
     num3 += 1;
     return num3;
-  } else if (num1 === 0 || num2 === 0 || num3 === 0) {
-    return 'Error';
-  } else {
+  } else 
     return false;
   }
 }
@@ -164,7 +164,21 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
- 
+ if (numero < 2) {
+   return false;
+ }
+
+ if (numero === 2) {
+   return true;
+ }
+
+ for (let i=2; i<numero; i++) {
+   if (numero % i === 0) {
+     return false;
+   } 
+ }
+
+ return true;
 }
 
 function esVerdadero(valor){
